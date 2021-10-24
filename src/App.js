162efe905f63bacdca3633983f272c7a8fb29847
rohan <rohannak2057@gmail.com>
2021-10-24 21:01:26 +0530

    cleared

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import Home from "./pages/home/Home"
-// import Login from "./pages/login/Login";
-// import Profile from "./pages/profile/Profile";
-// import Register from "./pages/register/Register";
-export default function App() {
-    return <Home />
-}
