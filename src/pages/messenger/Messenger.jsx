36a68d5c6c8063858e4b8ab0e56a4aec8a417f29
rohan <rohannak2057@gmail.com>
2021-12-03 13:34:26 +0530

    updated messenger

--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -3,7 +3,21 @@ import Topbar from "../../components/topbar/Topbar";
 import Converstion from "../../components/conversations/Converstion";
 import Message from "../../components/message/Message";
 import ChatOnline from "../../components/chatOnline/ChatOnline";
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
 export default function Messenger() {
+  const [conversations, setconversations] = useState([]);
+  const { user } = useContext(AuthContext);
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await axios.get("/conversations/" + user._id);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  }, [user._id]);
   return (
     <>
       <Topbar />
