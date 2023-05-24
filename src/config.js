f14cc366fec29990d7fa7e0cf705bd9628cde521
Rohan Kumar <rohannak2057@gmail.com>
2023-05-24 12:40:13 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "https://societal-being-backend.herokuapp.com/api/"
+   // baseURL: "https://societal-being-backend.herokuapp.com/api/"
+    baseURL: "https://societal-being-api.onrender.com/api/"
 })
